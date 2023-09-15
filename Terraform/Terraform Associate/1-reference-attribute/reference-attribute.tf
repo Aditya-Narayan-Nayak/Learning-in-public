269fe29f13af01f8e5b7406a577024c74d0058c9
AdityaNarayanNayak <ishaan4488@gmail.com>
2023-09-15 12:45:50 +0530

    Terraform learning

---
+++ Terraform/Terraform Associate/1-reference-attribute/reference-attribute.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIASR6IOZCLMGBU54VS"
+  secret_key = "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"
+}
+
+resource "aws_eip" "lb" {
+  domain   = "vpc"
+}
+
+# resource "aws_security_group" "allow_tls" {
+#   name        = "allow_tls"
+#   description = "Allow TLS inbound traffic"
+
+#   ingress {
+#     description      = "TLS from VPC"
+#     from_port        = 443
+#     to_port          = 443
+#     protocol         = "tcp"
+#     cidr_blocks      = ["${aws_eip.lb.public_ip}/32"]
+#   }
+
+#   egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
+
+#   tags = {
+#     Name = "allow_tls"
+#   }
+# }
+
+output "public_ip" {
+  value = aws_eip.lb.public_ip
+}
